c4e4de812cd8281574ab99d1d7e7f753c4efa72a
Bermet <iskenderova.bermet19@gmail.com>
2024-11-27 23:29:50 +0600

    UseState for keeping the list created

---
+++ src/filterComponent.jsx
@@ -0,0 +1,6 @@
+import React, {useState, useEffect} from 'react';
+const filterComponent = ()=> {
+    const [data, setData] = useState ([]);
+    const [searchTerm, setSearchTerm]=  useState('');
+    const [filteredData, setFilteredData] = useState([]);
+}
